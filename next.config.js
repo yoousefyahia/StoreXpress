26e7fca662df5576161d59f78fc21cb00fb4233e
yousef yahia <yoousefyahia@gmail.com>
2025-04-08 10:36:36 +0200

    next

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {}
+
+module.exports = nextConfig
